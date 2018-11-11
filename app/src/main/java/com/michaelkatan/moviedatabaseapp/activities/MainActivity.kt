d55639532c45e977047db5c879bb054e5c54ca46
katanmichael <michaelkatan@gmail.com>
2018-11-11 20:15:09 +0200

    Initial commit

---
+++ app/src/main/java/com/michaelkatan/moviedatabaseapp/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.michaelkatan.moviedatabaseapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.michaelkatan.moviedatabaseapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
